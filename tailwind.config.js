fac60007468f8aa1357d6bc5d1f80139d8301297
Oluwatobi <afopematilukuro@gmail.com>
2023-10-20 21:12:01 -0400

    SetUp Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        brand: {
+          "light-60": "rgba(229,229,229,60%)",
+          light: "rgb(229,229,229)",
+          dark: "rgb(34,40,52)",
+          "dark-50": "rgb(34,40,52,50%)",
+          "dark-80": "rgb(34,40,52,80%)",
+          blue: "rgb(68,171,229)",
+          background: "rgb(27,28,32)",
+        },
+      },
+      fontFamily: {
+        shrik: ["var(--shrik)"],
+        oswald: ["var(--oswald)"],
       },
     },
   },
   plugins: [],
-}
+};
